275d53a75e5a0f2b5a1f5a5df9bfdc822a0e40d8
dzung.apr6th@gmail.com <dzung.apr6th@gmail.com>
2022-02-20 18:38:31 +0700

    FirstCommit

---
+++ ProjectManagement/Model/StudentProjectApplyDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+namespace ProjectManagement.Model
+{
+    class StudentProjectApplyDBContext:DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
